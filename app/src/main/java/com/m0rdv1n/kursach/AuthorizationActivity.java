74bf1fb9c7a0fb1b822a6177d1c8dcef786a13be
m0rdv1n <NhDZq4T4jZ>
2022-02-18 14:58:17 +0300

    make authorization

--- app/src/main/java/com/m0rdv1n/kursach/AuthorizationActivity.java
+++ app/src/main/java/com/m0rdv1n/kursach/AuthorizationActivity.java
@@ -1,14 +1,64 @@
 package com.m0rdv1n.kursach;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class AuthorizationActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText email;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authorization);
+        mAuth = FirebaseAuth.getInstance();
+        email = (EditText) findViewById(R.id.inputEmailSignIn);
+        password = (EditText) findViewById(R.id.inputPasswordSignIn);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    public void oldUser(View v) {
+        authorization(email.getText().toString(), password.getText().toString());
+    }
+
+    private void authorization(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(AuthorizationActivity.this, "Authentication success.",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(AuthorizationActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
 }
